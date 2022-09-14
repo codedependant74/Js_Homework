aa835aaee1a31be3a10c1e8561cc0e4f29c38350
codedependant74 <brendale74@gmail.com>
2022-09-13 23:19:51 -0400

    Wild Wild Life answered

--- answers.js
+++ answers.js
@@ -17,23 +17,37 @@ for (i = 0; i <= 200; i++) {
 ////////////////////////////////
 // Fizz Buzz
 ////////////////////////////////
-for (let i = 1; i <= 100; i++) {
-  if (i % 3 == 0 && i % 5 == 0) {
-    console.log(`FizzBuzz`);
-  }
-  if (i % 3 == 0) {
-    console.log(`Fizz`);
-  }
-  if (i % 5 == 0) {
-    console.log(`Buzz`);
-  } else {
-    console.log(i);
-  }
-}
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 == 0 && i % 5 == 0) {
+//     console.log(`FizzBuzz`);
+//   }
+//   if (i % 3 == 0) {
+//     console.log(`Fizz`);
+//   }
+//   if (i % 5 == 0) {
+//     console.log(`Buzz`);
+//   } else {
+//     console.log(i);
+//   }
+// }
 ////////////////////////////////
 // Wild Wild Life
 ////////////////////////////////
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
+const sharky = ["Sharky", "shark", 20, "Left Coast"];
+const plantee = ["Plantee", "plant", 5000, "Mordor"];
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"];
+const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"];
 
+plantee[2]++;
+console.log(plantee);
+wolfy[3] = "Gotham City";
+console.log(wolfy);
+dart.push("Hawkins");
+console.log(dart);
+porgee.shift();
+porgee.unshift("Gameboy");
+console.log(porgee);
 ////////////////////////////////
 // Yell at the Ninja Turtles
 ////////////////////////////////
